baebf1aae69aaec83ba2e25d93bb2b280913647a
chitchanok <bowfriday@gmail.com>
2023-08-23 13:09:53 +0700

    second time

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,28 @@
-console.log('hihi')
+let express = require('express')
+let bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+require('./routes')(app)
+
+app.get('/status', function (req, res){
+  res.send('hello nodejs')
+})
+
+app.get('/hello/:person', function (req, res) {
+  console.log('Hello - ' + req.params.person)
+  res.send('say hello with ' + req.params.person)
+})
+
+app.post('/hello', function (req, res) {
+  res.send('OK you post - ' + req.body.name)
+})
+
+let port = 8081
+
+app.listen(port, function () {
+  console.log('server running on ' + port)
+})
